0fe4430c87e465ff86367fa0cb5498f26c34cfc4
DavidTron5000 <35479789+DavidTron5000@users.noreply.github.com>
2018-08-03 09:47:30 -0700

    Update send-text.js

--- functions/send-text.js
+++ functions/send-text.js
@@ -5,9 +5,8 @@ exports.handler = (event, context, callback) => {
    return callback(null, {
       statusCode: 200,
       body: JSON.stringify({
-        message: process.env.TWILIO_PHONE_NUMBER,
+        message: process.env,
       })
    })  
-
 
 }
